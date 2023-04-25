bd9f6364d788982122a877756e58d05f0bdb071b
Gustavo Bazzo <gcbazzo@gmail.com>
2023-04-25 13:06:44 -0400

    Update create-link.bat

--- create-link.bat
+++ create-link.bat
@@ -1,5 +1,12 @@
+rem The purpose of this file is to make programming the lib and Moodle easier. This way, the lib will be compiled and at the same time, the tool_htmlbootstrapeditor plugin will be able to get the compiled files.
+rem This file will be located in the @plugin@/react/node_modules/@recit/html-bootstrap-editor folder
+rem It will create a link from @plugin@/react/.../dist to the lib folder /html-bootstrap-editor/dist
+
 echo off
 set pluginPath=..\..\..\..\..\..\html-bootstrap-editor\dist
+
+rem remove the dist folder
+rmdir dist
 
 rem remove the current link
 ..\..\..\..\..\..\outils\junction -d dist
